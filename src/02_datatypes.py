bd89bf9efadd7bbd287eae942d5ea839381f854f
Jfadelli <jfadelli@gmail.com>
2020-08-31 13:39:53 -0700

    mvp-monday

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,15 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+print('This number should be 12')
+x = 5
+y = 7
+print(x+y)
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+print('This number should be 57')
+x = '5'
+y = '7'
+print(x+y)
--- src/03_modules.py
+++ src/03_modules.py
@@ -4,28 +4,40 @@ which allows you to access many system specific variables and
 methods, and the os module, which gives you access to lower-
 level operating system functionality.
 """
-
-import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
-
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-
+import sys 
+print("This is the name of the program:", sys.argv[0]) 
+print("Argument List:", str(sys.argv)) 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print('********************************************')
+import os
+import platform
+print('Operating System =', os.name, '| Platform =', platform.system(), platform.release())
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
+print('********************************************')
+import sys
+print("Python version")
+print (sys.version)
+print("Version info.")
+print (sys.version_info)
+print('********************************************')
 
-
-import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
-
 # Print the current process ID
 # YOUR CODE HERE
+print('Process ID = ', os.getpid())
+print('********************************************')
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-
+print('Current Working Directory = ', os.getcwd())
+print('********************************************')
 # Print out your machine's login name
 # YOUR CODE HERE
+import getpass
+print('Current User = ', getpass.getuser())
